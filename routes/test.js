1122eb4e94473f01b0d537c06cc030615b09ce65
Wee Kiat <ahkiat94@hotmail.com>
2018-03-19 20:10:24 +0800

    test1

--- routes/test.js
+++ routes/test.js
@@ -7,10 +7,32 @@ router.get('/', async function (req, res, next) {
     try {
         var connection = await qp.connectWithTbegin();
         var result_result = await qp.execute('SELECT * FROM ssad_db.Incidents', [], connection);
-        res.json(result_result);
-        var result_result2 = await qp.execute('SELECT * FROM ssad_db.Users', [], connection);
-        console.log(result_result2)
         await qp.commitAndCloseConnection(connection);
+        res.json(result_result);
+    }
+    catch (error) {
+        qp.rollbackAndCloseConnection(connection);
+        error.status = 406;
+        next(error);
+    }
+});
+
+router.post('/create', async function (req, res, next) {
+    try {
+        var connection = await qp.connectWithTbegin();
+        var result_insert = await qp.execute('insert into ssad_db.Users set ?', [req.body], connection);
+        let result = {};
+        result.affectedRows = result_insert.affectedRows;
+        result.changedRows = result_insert.changedRows;
+        result.fieldCount = result_insert.fieldCount;
+        result.insertId = result_insert.insertId;
+        result.message = result_insert.message;
+        result.protocol41 = result_insert.protocol41;
+        result.serverStatus = result_insert.serverStatus;
+        result.warningCount = result_insert.warningCount;
+        result.success = true;
+        await qp.commitAndCloseConnection(connection);
+        res.json(result);
     }
     catch (error) {
         qp.rollbackAndCloseConnection(connection);
