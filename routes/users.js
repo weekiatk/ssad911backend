1325f0c1ebab12bf336252ed35fee4ca93b68f21
Wee Kiat <ahkiat94@hotmail.com>
2018-03-20 14:14:20 +0800

    incidents

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,59 @@
 var express = require('express');
 var router = express.Router();
+var qp = require('flexqp-transaction');
+qp.presetConnection(require('../dbconfig.json'));
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function (req, res, next) {
+    try {
+        var connection = await qp.connectWithTbegin();
+        var result_result = await qp.execute('SELECT * FROM ssad_db.Users', [], connection);
+        await qp.commitAndCloseConnection(connection);
+        res.json(result_result);
+    }
+    catch (error) {
+        qp.rollbackAndCloseConnection(connection);
+        error.status = 406;
+        next(error);
+    }
+});
+
+router.post('/login', async function (req, res, next) {
+  try {
+      var connection = await qp.connectWithTbegin();
+      var result_result = await qp.execute('SELECT * FROM ssad_db.Users WHERE username = ?', [req.body.username], connection);
+      await qp.commitAndCloseConnection(connection);
+      res.json(result_result);
+  }
+  catch (error) {
+      qp.rollbackAndCloseConnection(connection);
+      error.status = 406;
+      next(error);
+  }
+});
+
+router.post('/create', async function (req, res, next) {
+    try {
+        var connection = await qp.connectWithTbegin();
+
+        var result_insert = await qp.execute('insert into ssad_db.Users set ?', [req.body], connection);
+        let result = {};
+        result.affectedRows = result_insert.affectedRows;
+        result.changedRows = result_insert.changedRows;
+        result.fieldCount = result_insert.fieldCount;
+        result.insertId = result_insert.insertId;
+        result.message = result_insert.message;
+        result.protocol41 = result_insert.protocol41;
+        result.serverStatus = result_insert.serverStatus;
+        result.warningCount = result_insert.warningCount;
+        result.success = true;
+        await qp.commitAndCloseConnection(connection);
+        res.json(result);
+    }
+    catch (error) {
+        qp.rollbackAndCloseConnection(connection);
+        error.status = 406;
+        next(error);
+    }
 });
 
 module.exports = router;
